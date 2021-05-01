6aabb91ad6e6f5f9563d7036d583a576197c68d6
Sreekaran Srinath <sreekaran.srinath@gmail.com>
2021-05-02 05:26:40 +0530

    Added Location Filteras

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
+from json.decoder import JSONDecodeError
 import requests
-import re
-import json
 from playsound import playsound
+from datetime import datetime as dt
+import time 
 
-# while 1:
-response = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=571&date=01-05-2021")
-response = json.loads(response.text)
-num_centers = len(response['centers'])
-for center in response['centers']:
-	centerDeets = {
-		"Name": center["name"], 
-		"Pincode": center["pincode"]
-	}
-	sessionList = center["sessions"]
-	for session in sessionList:
-		sessionDeets = {
-			"Date": session["date"], 
-			"Capacity": session["available_capacity"]
-		}
-		if session["min_age_limit"] == 18 and session["available_capacity"] > 0:
-			print(centerDeets, sessionDeets)
+while 1:
+	print(dt.now())
+	response = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=571&date=01-05-2021").json()
+	print(response)
+	try:
+		for center in response['centers']:
+			centerDeets = {
+				"Name": center["name"], 
+				"Pincode": center["pincode"]
+			}
+			sessionList = center["sessions"]
+			for session in sessionList:
+				sessionDeets = {
+					"Date": session["date"], 
+					"Capacity": session["available_capacity"]
+				}
+				if session["min_age_limit"] == 18 and session["available_capacity"] > 0 and ('UPHC' in center["name"] or 'VIJAYA' in center["name"] or 'UCHC' in center["name"] or 'APOLLO' in center["name"]):
+					print(centerDeets, sessionDeets)
+					playsound("./suffer.mp3")
+	except Exception as e:
+		print(e)
+	time.sleep(1)
